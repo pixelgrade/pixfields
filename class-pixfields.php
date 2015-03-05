52290e1e735eacef4cb9e887a2f476e76d07d5ff
andreilupu <andreilupu@pixelgrade.com>
2015-03-05 16:21:27 +0200

    0.5.3

--- class-pixfields.php
+++ class-pixfields.php
@@ -20,7 +20,7 @@ class PixFieldsPlugin {
 	 * @since   1.0.0
 	 * @const   string
 	 */
-	protected $version = '0.5.2';
+	protected $version = '0.5.3';
 
 	/**
 	 * Unique identifier for your plugin.
